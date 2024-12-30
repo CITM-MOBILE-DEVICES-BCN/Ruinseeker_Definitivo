5dddd13c3d8be74006e4f97252c6342928edae78
D1dii <didacgp123@gmail.com>
2024-12-30 11:55:33 +0100

    Scripts + Assemblies + LoadingBar

---
+++ Ruinseeker_Definitivo/Assets/Scripts/General/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using NavigationSystem;
+
+namespace Ruinseeker
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager instance;
+        public GameObject canvasForInstantiations;
+        public NavigationController navigationController;
+        
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+
+        private void Start()
+        {
+           
+        }
+
+        public void LoadSceneRequest(string sceneName)
+        {
+            navigationController.LoadScene(sceneName);
+        }
+
+        public void LoadScreenRequest(string screenName)
+        {
+            navigationController.LoadScreen(screenName, canvasForInstantiations.transform);
+        }
+
+        public void DestroyScreenRequest(string screenName)
+        {
+            navigationController.DestroyScreen(screenName);
+        }
+    }
+}
