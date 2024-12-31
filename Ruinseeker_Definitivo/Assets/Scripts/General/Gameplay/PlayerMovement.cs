e2abcf8425b25bc542e5b35435961abfedfe4d29
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2024-12-31 18:09:10 +0100

    walljump update, hay algun bug que otro

--- Ruinseeker_Definitivo/Assets/Scripts/General/Gameplay/PlayerMovement.cs
+++ Ruinseeker_Definitivo/Assets/Scripts/General/Gameplay/PlayerMovement.cs
@@ -8,11 +8,16 @@ namespace Ruinseeker
     {
         public float jumpForce = 10f; // Fuerza del salto
         public float moveSpeed = 2f; // Velocidad de movimiento
+        public float wallJumpForce = 5f; // Fuerza del salto en la pared
+        public float wallJumpForceX = 5f;
+        public float wallSlideSpeed = 1f; // Velocidad de deslizamiento en la pared
         private Rigidbody2D rb;
         private Vector3 moveDirection = Vector3.right; // Dirección inicial de movimiento
         private Vector3 originalPosition;
         private Quaternion originalRotation;
         bool hasJumped = false;
+        bool isTouchingWall = false;
+        bool isGrounded = false;
 
         // Start is called before the first frame update
         void Start()
@@ -25,19 +30,36 @@ namespace Ruinseeker
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) && !hasJumped)
             {
-                Jump();
+                if (isTouchingWall)
+                {
+                    WallJump();
+                }
+                else
+                {
+                    Jump();
+                }
             }
 
-            Move();
+            if (!isTouchingWall)
+            {
+                Move();
+            }
         }
 
         void Jump()
         {
-
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             hasJumped = true;
+        }
+
+        void WallJump()
+        {
+            // Aplicar fuerza en la dirección opuesta a la pared y hacia arriba
+            rb.velocity = new Vector2(wallJumpForceX * (transform.localScale.x > 0 ? 1 : -1), jumpForce);
+            hasJumped = true;
+            isTouchingWall = false; // Resetear el estado de contacto con la pared
         }
 
         void Move()
@@ -50,15 +72,42 @@ namespace Ruinseeker
             if (collision.gameObject.CompareTag("Wall"))
             {
                 moveDirection = moveDirection == Vector3.right ? Vector3.left : Vector3.right;
+                if (!isGrounded)
+                {
+                    isTouchingWall = true;
+                    hasJumped = false;
+                    rb.velocity = new Vector2(0, -wallSlideSpeed); // Deslizarse lentamente hacia abajo
+                }
             }
             else if (collision.gameObject.CompareTag("Ground"))
             {
+                isGrounded = true;
                 // Restaurar el estado original del personaje
                 transform.position = new Vector3(transform.position.x, originalPosition.y, transform.position.z);
                 transform.rotation = originalRotation;
                 rb.velocity = Vector3.zero;
+                hasJumped = false; // Resetear el estado de salto
+            }
+        }
+
+        void OnCollisionStay2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Wall") && !isGrounded)
+            {
+                rb.velocity = new Vector2(0, -wallSlideSpeed); // Deslizarse lentamente hacia abajo
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Wall"))
+            {
+                isTouchingWall = false;
+            }
+            else if (collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = false;
             }
         }
     }
 }
-
